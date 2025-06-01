2760a861428da9d379484b09ab5bafe8ed1f57c4
engeles20 <erick.angeles@gmail.com>
2025-06-01 00:05:11 -0500

    new links

---
+++ upload.bat
@@ -0,0 +1,13 @@
+timeout 2 >> NUL
+pushd C:\Users\erickkkk\Documents\GitHub\gta-rp-playlist.github.io
+timeout 2 >> NUL
+pushd C:\Users\erickkkk\Documents\GitHub\gta-rp-playlist.github.io
+timeout 2 >> NUL
+git add -A
+timeout 3 >> NUL
+git commit -m "new links"
+timeout 3 >> NUL
+git pull
+timeout 3 >> NUL
+git push
+timeout 3 >> NUL
