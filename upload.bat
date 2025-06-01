f9c10a9e8198471460a343baa6a244b3da78dcf4
engeles20 <erick.angeles@gmail.com>
2025-06-01 00:05:50 -0500

    new links

--- upload.bat
+++ upload.bat
@@ -1,11 +1,9 @@
-timeout 2 >> NUL
-timeout 2 >> NUL
-timeout 2 >> NUL
+timeout 3
 git add -A
-timeout 3 >> NUL
+timeout 3 
 git commit -m "new links"
-timeout 3 >> NUL
+timeout 3 
 git pull
-timeout 3 >> NUL
+timeout 3
 git push
-timeout 3 >> NUL
+timeout 3
