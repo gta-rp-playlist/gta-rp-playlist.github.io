7ead38848a21afdb3e1f2c0ff74044cf8e0592b3
gta-rp-playlist <erick.ang.eles@gmail.com>
2025-06-04 20:10:05 -0500

    Update filter_main.js

--- filter_main.js
+++ filter_main.js
@@ -130,7 +130,7 @@ document.addEventListener("DOMContentLoaded", async function () {
                     width: "100%",
                     height: "100%",
                     channel: username,
-                    parent: ["127.0.0.1", "gta-rp-playlist.github.io"],
+                    parent: ["127.0.0.1", "gta-rp-playlist.com"],
                     muted: index !== 0,
                     layout: "video",
                 });
