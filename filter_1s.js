c59849ae98f8c739bf6009c8c95b8099a1753b5b
gta-rp-playlist <erick.ang.eles@gmail.com>
2025-06-04 20:09:28 -0500

    Update filter_1s.js

--- filter_1s.js
+++ filter_1s.js
@@ -70,7 +70,7 @@ function showCategoryStreams(category, streams) {
                 width: '100%',
                 height: '100%',
                 channel: username,
-                parent: ["127.0.0.1", "gta-rp-playlist.github.io"],
+                parent: ["127.0.0.1", "gta-rp-playlist.com"],
                 muted: false,
                 layout: 'video'
             });
