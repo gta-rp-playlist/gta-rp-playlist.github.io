955e5187eba2e452db145f0e8ba4dd4ae59f6198
gta-rp-playlist <erick.ang.eles@gmail.com>
2025-07-30 21:04:20 -0500

    Add files via upload

--- start_1.js
+++ start_1.js
@@ -1,5 +1,9 @@
 // this filename is start_1.js
-
+document.addEventListener("DOMContentLoaded", function () {
+    const survivalItem = document.createElement("li");
+    survivalItem.innerHTML = `<a href="https://gta-rp-playlist.com/survival.html">Survival S7</a>`;
+    document.querySelector("#top-menu ul").prepend(survivalItem);
+});
 function sortCategories() {
     var sidebar = document.getElementById('sidebar');
     var categories = Array.from(sidebar.getElementsByClassName('category'));
